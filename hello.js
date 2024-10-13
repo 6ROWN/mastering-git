2f27e98337197130edc56c55fff4079ea7e9f467
Daniel Gohoho <gohohodanni@gmail.com>
2024-10-13 22:19:29 +0000

    chore: modify the hello js file to contain greetings

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
 console.log("Made some changes from the featured branch");
+
+const greetings = (name, age) => {
+	console.log(`Hello ${name}, I am ${age} years old`);
+};
+
+greetings("daniel", 34);
